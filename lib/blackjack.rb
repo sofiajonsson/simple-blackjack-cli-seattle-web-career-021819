c58deb0cc72fde46f0e1f5325a03b0bea5d378f3
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-17 05:42:27 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -3,11 +3,8 @@ def welcome
   puts "Welcome to the Blackjack Table"
 end
 
-def deal_card (card)
-  # code #deal_card here
-  card > 0
-   card.rand(1..11) 
- 
+def deal_card
+  card = rand(1..11) 
 end
 
 def display_card_total (card_total)
@@ -16,37 +13,60 @@ def display_card_total (card_total)
 end
 
 def prompt_user
-  # code #prompt_user here
-  gets.chomp 
-  
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
-  expect(self).to receive(:get_user_input).and_return("s")
+  user_input = gets.chomp
 end
 
-def end_game
-  # code #end_game here
+
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  card_one = deal_card
+  card_two = deal_card
+  card_total = card_one + card_two
+  display_card_total(card_total)
+  return card_total
 end
 
-def hit?
-  # code hit? here
+
+def hit?(card_total)
+  prompt_user #asking user if they want to hit or stay
+  user_input = get_user_input #sets a var to use for  if/else statement
+  if user_input == "h"
+    card_total += deal_card #Incraments the prior total by the new number dealt by dealing another card
+  elsif user_input == "s"
+    card_total #return the number and prompt user to hit again
+  else
+    invalid_command
+  end
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
+ 
 end
+
 
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
 
+
 def runner
-  # code runner here
+  welcome #introduces the game
+  
+  card_total = initial_round #est. current total + lets user know what number they have at the moment
+  until card_total > 21 #continue game until user loses
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total) #prints out when user has lost
 end
+
+
     
